eaf11ac24a39f0f46b849d904db1cc7d3fff0b71
TCMudiwa <mudiwa.tino@gmail.com>
2022-03-14 13:54:00 -0700

    task 4 betty considerations

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main- pritns the phrase programming is like buiding ....
+ * Return: 0 if exiyited properly
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
- return 0;
+return (0);
 }
