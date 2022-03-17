d3f93b62d3c3e173c52a0b653ca250779cf0defe
TCMudiwa <mudiwa.tino@gmail.com>
2022-03-17 11:54:33 -0700

    task 3 conditional return

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*int is_lower(int c) - checks if character is a lower case character
+*Return: 1 if lowercase 
+*0 if otherwise
+*/
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+return(1);
+else
+return (0)
+}
+
+  
