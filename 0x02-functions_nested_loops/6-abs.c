f9d0ef97513a65f29d405ec103166d944a6a75b8
TCMudiwa <mudiwa.tino@gmail.com>
2022-03-17 13:22:27 -0700

    betty style

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * _abs(int) - a function that computes the absolute absolute value of an int.
+ * _abs - a function that computes the absolute absolute value of an int.
  * Return: int in absloute form. Zero is correct
  *@a: number to check
  */
 int _abs(int a)
 {
-    if (a < 0)
-      {
-       return (-a);
-      }
-    else
-      {
-       return (a);
-      }
+if (a < 0)
+{
+return (-a);
+}
+else
+{
+return (a);
+}
 }
