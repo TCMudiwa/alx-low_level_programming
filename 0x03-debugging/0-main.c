7577a1acf777ea48bd5c217720bcf141cec15814
TCMudiwa <mudiwa.tino@gmail.com>
2022-03-21 03:42:15 -0700

    task 0 test case for positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *main - tests positive_or_negative, function checks if integers are + or -
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
