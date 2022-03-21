81f4201af74540ee5f02eacadafa2ed51fb259e9
TCMudiwa <mudiwa.tino@gmail.com>
2022-03-21 03:50:49 -0700

    changed description notes to match task

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- *main - tests positive_or_negative, function checks if integers are + or -
+ *main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 int main(void)
 {
 int i;
-i = -98;
+i = 0;
 positive_or_negative(i);
 return (0);
 }
