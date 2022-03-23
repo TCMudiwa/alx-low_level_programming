a3110e176e8c92e9cc5b5109f5fef0a2325000b2
TCMudiwa <mudiwa.tino@gmail.com>
2022-03-23 11:31:27 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_most_numbers - prints numbers one to nine except two and four
+ *Return: void
+ */
+void print_most_numbers(void)
+{
+int i;
+i = '0';
+while (i <= '9' && !'2' || !'4')
+{
+_putchar(i);
+}
+_putchar('\n');
+return;
+}
