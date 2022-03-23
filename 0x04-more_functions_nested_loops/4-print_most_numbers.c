3e6e2249c5df70adc09e8119d5781f3466ee0d92
TCMudiwa <mudiwa.tino@gmail.com>
2022-03-23 12:08:51 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,13 +5,14 @@
  */
 void print_most_numbers(void)
 {
-  int i;
-  i = 0;
-  while (i <= 9)
+int i;
+i = 0;
+while (i <= 9)
 {
 if (i != 2 && i != 4)
 _putchar(i);
+i++
 _putchar('\n');
- }
+}
  return;
 }
