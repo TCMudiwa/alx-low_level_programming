333d3cf4b0f7ce5f1a626645f8bf0cafb4650c45
TCMudiwa <mudiwa.tino@gmail.com>
2022-04-11 09:05:52 -0700

    task 8 print n elements of an array of integers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ int _putchar(char);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 void _puts(char *str);
-int _strlen(char *s); 
+int _strlen(char *s);
+void print_array(int *a, int n);
 #endif
