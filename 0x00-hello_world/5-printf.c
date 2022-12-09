796db34ab721566ac7470069a8a5393b2e2d78a0
TCMudiwa <mudiwa.tino@gmail.com>
2022-12-09 16:18:52 +0300

    task 5 forgot semi colons

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 /**
 *main - prints a phrase
@@ -5,6 +6,6 @@
 **/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0)
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
