09074dfae7c10b07c5742c343961b2ad0326bae6
TCMudiwa <mudiwa.tino@gmail.com>
2022-12-09 15:58:20 +0300

    task 4 puts.c had to figure out betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-/* Programme that prints a phrase*/
-int main()
+/**
+*main - Programme that prints a phrase
+*Return: Always 0
+*/
+int main(void)
 {
-  char Mystr[]= "Programming is like building a multilingual puzzle/n";
+char Mystr[] = "Programming is like building a multilingual puzzle\n";
 puts(Mystr);
- return(0);
+return (0);
 }
