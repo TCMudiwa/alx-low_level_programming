2fd34502865ef4524434911e367b6e0355a02398
TCMudiwa <mudiwa.tino@gmail.com>
2022-12-13 16:19:58 +0300

    experimenting with gets

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - function that prints the quote
+*Return: Always One if Successful
+ **/
+int main(void)
+{
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+gets(str);
+return (1);
+}
