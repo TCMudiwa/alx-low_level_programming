d6c336a9c21a91a6943224e9c4ac74e3f84a1880
TCMudiwa <mudiwa.tino@gmail.com>
2022-12-13 16:39:14 +0300

    task 7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
 *main - function that prints the quote
 *Return: Always One if Successful
  **/
 int main(void)
 {
-fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 12;
 return (1);
 }
