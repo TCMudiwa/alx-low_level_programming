983048997afb5ba7ebecb7f8866e53e6dad72f84
TCMudiwa <mudiwa.tino@gmail.com>
2022-12-13 15:49:12 +0300

    task 6 correcting order of output

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ float floattype;
 printf("Size of a char: %zu byte(s)\n", sizeof(chartype));
 printf("Size of a int:  %zu byte(s)\n", sizeof(inttype));
 printf("Size of a long int: %zu byte(s)\n", sizeof(longtype));
-printf("Size of a float: %zu byte(s)\n", sizeof(floattype));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongtype));
+printf("Size of a float: %zu byte(s)\n"n sizeof(floattype);
 return (0);
 }
