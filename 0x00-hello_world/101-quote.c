ce7ede3651f62529e86db7cf79ac0539ac444e48
TCMudiwa <mudiwa.tino@gmail.com>
2022-12-13 16:33:06 +0300

    trying write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,6 @@
  **/
 int main(void)
 {
-char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-gets(str);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 20;
 return (1);
 }
