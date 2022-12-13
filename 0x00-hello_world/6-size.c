eba64ffb53fafe54752e74c9308624137eddad53
TCMudiwa <mudiwa.tino@gmail.com>
2022-12-13 15:59:08 +0300

    task 6 with more white space

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ long long longlongtype;
 char chartype;
 float floattype;
 printf("Size of a char: %zu byte(s)\n", sizeof(chartype));
-printf("Size of an int:  %zu byte(s)\n", sizeof(inttype));
+printf("Size of an int: %zu byte(s)\n", sizeof(inttype));
 printf("Size of a long int: %zu byte(s)\n", sizeof(longtype));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongtype));
 printf("Size of a float: %zu byte(s)\n", sizeof(floattype));
