352754b4eb73bb4e00fd9e3f7cbc1f8b05a942cb
TCMudiwa <mudiwa.tino@gmail.com>
2022-12-21 21:28:54 +0300

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _isalpha - function checks if a char is cap letter
+* @c: char to be checked
+* Return: 1 if c is cap or 0 otherwise
+*/
+int _isalpha(int c)
+{
+if ((c >= 65) && (c <= 90))
+return (1);
+else if ((c >= 97) && (c <= 122))
+return (1);
+else
+return (0);
