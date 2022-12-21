93245637ea48e34b19642b106080c27dc27b7939
TCMudiwa <mudiwa.tino@gmail.com>
2022-12-21 20:54:08 +0300

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*main - check the code for school students
+* Return: Always 0 for success
+**/
+int main(void)
+{
+char s[] = "_putchar";
+int i = 0;
+while (s[i] != '\0')
+{
+_putchar(s[i]);
+i++;
+}
+_putchar(10);
+return (0);
+}
