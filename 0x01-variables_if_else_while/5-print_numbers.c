bab8361915bd82ba6f764da183d6424f9f70dd8b
TCMudiwa <mudiwa.tino@gmail.com>
2022-12-21 15:56:45 +0300

    task 5 print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - prints alphabet in lowercase
+* Return: 0 if successful
+**/
+int main(void)
+{
+int alphabet;
+for (alphabet = 0; alphabet <= 9; alphabet++)
+{
+printf("%d", alphabet);
+}
+printf("\n");
+return (0);
+}
