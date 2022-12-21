ed8bfa4f8a7e27c531a9b4773938a206840471dd
TCMudiwa <mudiwa.tino@gmail.com>
2022-12-21 16:13:12 +0300

    task 8 hexadecimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - prints alphabet in lowercase
+* Return: 0 if successful
+**/
+int main(void)
+{
+int alphabet;
+for (alphabet = 48; alphabet <= 57 ; alphabet++)
+{
+putchar(alphabet);
+}
+for (alphabet = 97; alphabet <= 102 ; alphabet++)
+{
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
