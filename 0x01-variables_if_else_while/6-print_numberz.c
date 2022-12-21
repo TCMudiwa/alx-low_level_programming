e289e1fc8a0d05a8fb75ee667cba3e9823bfb08b
TCMudiwa <mudiwa.tino@gmail.com>
2022-12-21 15:59:34 +0300

    putchar numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - prints alphabet in lowercase
+* Return: 0 if successful
+**/
+int main(void)
+{
+int alphabet;
+for (alphabet = '1'; alphabet <= '9' ; alphabet++)
+{
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
